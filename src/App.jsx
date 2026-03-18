eee423904b6989edba18ffa44d257a6e56083f12
Khalil <khalil.bm@hotmail.com>
2026-03-18 01:42:25 -0300

    feat: add canvas and initial drawing

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,23 @@
+import { useRef, useEffect } from "react";
+
 export default function App() {
-  return <h1>🚦 Traffic Game</h1>;
+  const canvasRef = useRef(null);
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext("2d");
+
+    canvas.width = 400;
+    canvas.height = 400;
+
+    ctx.fillStyle = "white";
+    ctx.fillRect(180, 180, 40, 40);
+  }, []);
+
+  return (
+    <>
+      <h1>🚦 Traffic Game</h1>
+      <canvas ref={canvasRef} />
+    </>
+  );
 }
