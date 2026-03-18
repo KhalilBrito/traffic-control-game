e5d64931afcca58637ee1bfb0ca52b2b50d4e787
Khalil <khalil.bm@hotmail.com>
2026-03-18 01:45:12 -0300

    feat: draw roads and intersection

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,27 @@ export default function App() {
   const canvasRef = useRef(null);
 
   useEffect(() => {
-    const canvas = canvasRef.current;
-    const ctx = canvas.getContext("2d");
+  const canvas = canvasRef.current;
+  const ctx = canvas.getContext("2d");
 
-    canvas.width = 400;
-    canvas.height = 400;
+  canvas.width = 400;
+  canvas.height = 400;
 
-    ctx.fillStyle = "white";
-    ctx.fillRect(180, 180, 40, 40);
-  }, []);
+  const center = canvas.width / 2;
+  const roadWidth = 80;
+
+  // fundo
+  ctx.fillStyle = "#111";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+  // rua horizontal
+  ctx.fillStyle = "#444";
+  ctx.fillRect(0, center - roadWidth / 2, canvas.width, roadWidth);
+
+  // rua vertical
+  ctx.fillRect(center - roadWidth / 2, 0, roadWidth, canvas.height);
+
+}, []);
 
   return (
     <>
